d4db2d4ee0d981da7a83b56c2b7bca4abd862575
Subhakanta Lenka <subhakanta.lenka@paritycube.com>
2025-11-08 23:02:32 +0530

    Fix file path for serving React index.html

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   def fallback_index_html
-    render file: 'public/index.html'
+    render file: Rails.public_path.join('index.html').to_s
   end
 end
